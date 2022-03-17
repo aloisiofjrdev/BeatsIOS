db684a2c840db20f92010a56d270fd4351a94182
Aloisio Formento Junior <aloisio.formento@NMAC-2932.local>
2022-03-17 10:18:26 -0300

    Big Bang

---
+++ BeatsIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeatsIOS
+//
+//  Created by Aloisio Formento Junior on 17/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
